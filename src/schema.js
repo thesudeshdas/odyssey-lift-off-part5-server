54cb75c98cbf14a1696eacd0309e573527ae7549
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-21 23:04:09 +0530

    schema

--- src/schema.js
+++ src/schema.js
@@ -35,8 +35,8 @@ const typeDefs = gql`
     author: Author!
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
-    "The track's approximate length to complete, in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The track's full duration, in seconds"
     durationInSeconds: Int
     "The number of modules this track contains"
@@ -63,8 +63,8 @@ const typeDefs = gql`
     id: ID!
     "The module's title"
     title: String!
-    "The module's length in minutes"
-    length: Int
+    "The module's length in seconds"
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's video duration, in seconds"
     durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
